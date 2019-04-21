07507be9e619c00657543ccca037dee969f3dc4d
Steven <sasilva1998@gmail.com>
2019-04-21 03:06:35 -0500

    resto de configuraciones

--- ov2640_config.py
+++ ov2640_config.py
@@ -1,3 +1,217 @@
+#light mode
+SUNNY=[
+[0xff, b'\x00'],
+[0xc7, b'\x40'],
+[0xcc, b'\x5e'],
+[0xcd, b'\x41'],
+[0xce, b'\x54']
+]
+
+CLOUDY=[
+[0xff, b'\x00'],
+[0xc7, b'\x40'],
+[0xcc, b'\x65'],
+[0xcd, b'\x41'],
+[0xce, b'\x4f']
+]
+
+OFFICE=[
+[0xff, b'\x00'],
+[0xc7, b'\x40'],
+[0xcc, b'\x52'],
+[0xcd, b'\x41'],
+[0xce, b'\x66']
+]
+
+HOME=[
+[0xff, b'\x00'],
+[0xc7, b'\x40'],
+[0xcc, b'\x42'],
+[0xcd, b'\x3f'],
+[0xce, b'\x71']
+]
+
+#color saturation
+
+SATURATION2=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x02'],
+[0x7c, b'\x03'],
+[0x7d, b'\x68'],
+[0x7d, b'\x68']
+]
+
+SATURATION1=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x02'],
+[0x7c, b'\x03'],
+[0x7d, b'\x58'],
+[0x7d, b'\x58']
+]
+
+SATURATION0=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x02'],
+[0x7c, b'\x03'],
+[0x7d, b'\x48'],
+[0x7d, b'\x48']
+]
+
+SATURATIONMN1=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x02'],
+[0x7c, b'\x03'],
+[0x7d, b'\x38'],
+[0x7d, b'\x38']
+]
+
+SATURATIONMN2=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x02'],
+[0x7c, b'\x03'],
+[0x7d, b'\x38'],
+[0x7d, b'\x38']
+]
+
+
+#brightness
+BRIGHTNESS2=
+[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x04'],
+[0x7c, b'\x09'],
+[0x7d, b'\x40'],
+[0x7d, b'\x00']
+]
+
+BRIGHTNESS1=
+[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x04'],
+[0x7c, b'\x09'],
+[0x7d, b'\x30'],
+[0x7d, b'\x00']
+]
+
+BRIGHTNESS0=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x04'],
+[0x7c, b'\x09'],
+[0x7d, b'\x20'],
+[0x7d, b'\x00']
+]
+
+BRIGHTNESSMN1=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x04'],
+[0x7c, b'\x09'],
+[0x7d, b'\x10'],
+[0x7d, b'\x00']
+]
+
+BRIGHTNESSMN2=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x04'],
+[0x7c, b'\x09'],
+[0x7d, b'\x00'],
+[0x7d, b'\x00']
+]
+
+
+#contrast
+CONTRAST2=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x04'],
+[0x7c, b'\x07'],
+[0x7d, b'\x20'],
+[0x7d, b'\x28'],
+[0x7d, b'\x0c'],
+[0x7d, b'\x06']
+]
+
+CONTRAST1=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x04'],
+[0x7c, b'\x07'],
+[0x7d, b'\x20'],
+[0x7d, b'\x24'],
+[0x7d, b'\x16'],
+[0x7d, b'\x06']
+]
+
+CONTRAST0=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x04'],
+[0x7c, b'\x07'],
+[0x7d, b'\x20'],
+[0x7d, b'\x20'],
+[0x7d, b'\x20'],
+[0x7d, b'\x06']
+]
+
+CONTRASTMN1=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x04'],
+[0x7c, b'\x07'],
+[0x7d, b'\x20'],
+[0x7d, b'\x1c'],
+[0x7d, b'\x2a'],
+[0x7d, b'\x06']
+]
+
+CONTRASTMN2=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x04'],
+[0x7c, b'\x07'],
+[0x7d, b'\x20'],
+[0x7d, b'\x18'],
+[0x7d, b'\x34'],
+[0x7d, b'\x06']
+]
+
+#special effects
+ANTIQUE=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x18'],
+[0x7c, b'\x05'],
+[0x7d, b'\x40'],
+[0x7d, b'\xa6']
+]
+
+BLUISH=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x18'],
+[0x7c, b'\x05'],
+[0x7d, b'\xa0'],
+[0x7d, b'\x40']
+]
+
+GREENISH=[
+[0xff, b'\x00'],
+[0x7c, b'\x00'],
+[0x7d, b'\x18'],
+[0x7c, b'\x05'],
+[0x7d, b'\x40'],
+[0x7d, b'\x40']
+]
+
 BW=[
 [0xff, b'\x00'],
 [0x7c, b'\x00'],
